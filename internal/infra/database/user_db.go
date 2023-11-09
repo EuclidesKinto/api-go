96c93e4b2c1ca09e6bdf57fbf9233e8c21eb24a3
euclides <euclideskinto@gmail.com>
2023-11-09 19:46:13 -0300

    Testando criação do usuário

--- internal/infra/database/user_db.go
+++ internal/infra/database/user_db.go
@@ -1,6 +1,9 @@
 package database
 
-import "gorm.io/gorm"
+import (
+	"api-go/internal/entity"
+	"gorm.io/gorm"
+)
 
 type User struct {
 	DB *gorm.DB
@@ -8,4 +11,17 @@ type User struct {
 
 func NewUser(db *gorm.DB) *User {
 	return &User{DB: db}
+}
+
+func (u *User) Create(user *entity.User) error {
+	return u.DB.Create(user).Error
+}
+
+func (u *User) FindByEmail(email string) (*entity.User, error) {
+	var user entity.User
+	err := u.DB.Where("email = ?", email).First(&user).Error
+	if err != nil {
+		return nil, err
+	}
+	return &user, nil
 }
